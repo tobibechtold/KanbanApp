a43de6ba216c4dfcfbdaab8f92ed3198851fa6d1
Tobi <tobi.bechtold@gmail.com>
2016-04-25 23:42:06 +0200

    Added basic Drag and Drop functionality for Notes.

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,67 +1,40 @@
 import React from 'react';
+import {DragSource, DropTarget} from 'react-dnd';
+import ItemTypes from '../constants/ItemTypes';
 
-export default class Note extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      editing: false
+const noteSource = {
+  beginDrag(props) {
+    return {
+      id: props.id
     };
   }
-  render() {
-    if(this.state.editing) {
-      return this.renderEdit();
-    }
+}
 
-    return this.renderNote();
+const noteTarget = {
+  hover(targetProps, monitor) {
+    const targetId = targetProps.id;
+    const sourceProps = monitor.getItem();
+    const sourceId = sourceProps.id;
+
+    if(sourceId !== targetId) {
+      targetProps.onMove({sourceId, targetId});
+    }
   }
-  renderEdit = () => {
-    return <input type="text"
-      ref={
-        element => element ?
-        element.selectionStart = this.props.task.length :
-        null
-      }
-      autoFocus= {true}
-      defaultValue={this.props.task}
-      onBlur={this.finishEdit}
-      onKeyPress={this.checkEnter} />;
-  };
-  renderNote = () => {
-    const onDelete = this.props.onDelete;
+};
 
-    return (
-    <div
-      onClick={this.edit}>
-      <span className="task">{this.props.task}</span>
-      {onDelete ? this.renderDelete() : null}
-    </div>
-  );
-  };
-  renderDelete = () => {
-    return <button
-      className="delete-note"
-      onClick={this.props.onDelete}>x</button>;
-  };
-  edit = () => {
-    this.setState({
-      editing: true
-    });
-  };
-  checkEnter = (e) => {
-    if(e.key === 'Enter') {
-      this.finishEdit(e);
-    }
-  };
-  finishEdit = (e) => {
-    const value = e.target.value;
+@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
+  connectDragSource: connect.dragSource()
+}))
+@DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
+  connectDropTarget: connect.dropTarget()
+}))
+export default class Note extends React.Component {
+  render() {
+    const {connectDragSource, connectDropTarget, id, onMove, ...props}
+      = this.props;
 
-    if(this.props.onEdit) {
-      this.props.onEdit(value);
-
-      this.setState({
-        editing: false
-      });
-    }
-  };
+    return connectDragSource(connectDropTarget(
+      <li {...props}>{props.children}</li>
+    ));
+  }
 }
--- app/stores/LaneStore.js
+++ app/stores/LaneStore.js
@@ -1,6 +1,7 @@
 import uuid from 'node-uuid';
 import alt from '../libs/alt';
 import LaneActions from '../actions/LaneActions';
+import update from 'react-addons-update';
 
 class LaneStore {
   constructor() {
@@ -59,6 +60,32 @@ class LaneStore {
 
       return lane;
     });
+
+    this.setState({lanes});
+  }
+  move({sourceId, targetId}) {
+    const lanes = this.lanes;
+    const sourceLane = lanes.filter(lane => lane.notes.includes(sourceId))[0];
+    const targetLane = lanes.filter(lane => lane.notes.includes(targetId))[0];
+    const sourceNoteIndex = sourceLane.notes.indexOf(sourceId);
+    const targetNoteIndex = targetLane.notes.indexOf(targetId);
+
+    if(sourceLane === targetLane) {
+      // move at once to avoid complications
+      sourceLane.notes = update(sourceLane.notes, {
+        $splice: [
+          [sourceNoteIndex, 1],
+          [targetNoteIndex, 0, sourceId]
+        ]
+      });
+    }
+    else {
+      // get rid of the source
+      sourceLane.notes.splice(sourceNoteIndex, 1);
+
+      // and move it to target
+      targetLane.notes.splice(targetNoteIndex, 0, sourceId);
+    }
 
     this.setState({lanes});
   }
